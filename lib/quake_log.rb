4c17975cec15f5206a09a68774d496bc3d013ba2
murari <murarii29sahuu@gmail.com>
2022-06-17 16:59:55 +0530

    add quake game display information

---
+++ lib/quake_log.rb
@@ -0,0 +1,61 @@
+require_relative 'log_utils'
+require_relative 'game'
+
+class QuakeLog
+  include LogUtils
+
+  def initialize(log_file = get_log_file_path)
+    @log_file = log_file
+    @game = []
+    @current_game = nil
+  end
+
+  def parse_log_file
+    read_from_file(@log_file) do |file|
+      file.each do |line|
+        if game_start?(line)
+          game_name = "game_#{@games.length + 1}"
+          @current_game = Game.new(game_name)
+        elsif player_line?(line)
+          player_name = get_player_name(line)
+          @current_game.add_player(player_name)
+        elsif kill_event?(line)
+          killer, killed, kill_reason = get_kill_info_from_kill_event(line)
+          kill_reason = kill_reason.to_sym
+          kill_reason = :MOD_UNKNOWN unless MEANS_OF_DEATH.include?(kill_reason)
+          @current_game.deal_with_kill_event(killer, killed, kill_reason)
+        elsif game_over?(line)
+          @games << @current_game if @current_game && !@games.include?(@current_game)
+        else
+          next
+        end
+      end
+    end
+  end
+
+  def display_game_information(game_name)
+    if game_name
+      puts get_game_by_name(game_name)
+    else
+      @games.each { |game| puts game }
+    end
+  end
+
+  def display_kill_reasons(game_name)
+    if game_name
+      game = get_game_by_name(game_name)
+      puts game.display_aggregation_kill_reasons
+    else
+      @games.each { |game| puts game.display_aggregation_kill_reasons }
+    end
+  end
+
+  def get_game_by_name(game_name)
+    game = @games.detect { |g| g.game_name == game_name }
+    return game if game
+
+    puts "Error: Game #{game_name} not found."
+    exit 1
+  end
+
+end
